3a37ce558cbc1975d9df8a353689efddfa452bb1
Orel Adivi <orel.adivi@cs.technion.ac.il>
2024-03-05 11:58:29 +0200

    Update experiment

--- experiments/Experiments.py
+++ experiments/Experiments.py
@@ -3,3 +3,28 @@
 #   @date : 05 March 2024
 #   @authors : Orel Adivi and Daniel Noor
 #
+import time
+import numpy as np
+from typing import Callable
+
+
+def measure_code(code_constructor: Callable) -> [int, int]:
+    deletion_results = []
+    time_results = []
+    for _ in range(3):
+        start_time = time.time()
+        deletions = code_constructor().max_deletions()
+        end_time = time.time()
+        deletion_results.append(deletions)
+        time_results.append(end_time - start_time)
+    return tuple([max(deletion_results), np.average(time_results)])
+
+
+def calculate_m_value(length: int) -> int:
+    log_l = int(np.ceil(np.log2(length)))
+    return log_l + int(np.ceil(np.log2(log_l))) + 1
+
+
+if __name__ == '__main__':
+    cc = 1
+    measure_code(cc)
