9684cf6d606ce4e50e1e1bf394b728e7bec9624a
Daniel Noor <63052638+DanielN162@users.noreply.github.com>
2024-03-20 14:44:21 +0200

    Add LCS unittest

--- experiments/Unittests.py
+++ experiments/Unittests.py
@@ -4,6 +4,7 @@
 #   @authors : Orel Adivi and Daniel Noor
 #
 import unittest
+import pylcs
 from utils.LevenshteinDistance import *
 from utils.LongestCommonSubsequence import *
 
@@ -41,4 +42,30 @@ class TestLevenshteinDistance(unittest.TestCase):
         self.assertTrue(levenshtein_deletion_distance('1010', '11010') == 1)
         self.assertTrue(levenshtein_deletion_distance('110001', '0000010') == 5)
         self.assertTrue(levenshtein_deletion_distance('110000', '0100010') == 3)
+
+
+class TestLCS(unittest.TestCase):
+    def test_random(self):
+        for i in range(10):
+            w1 = ''.join([str(x) for x in np.random.choice([0, 1], size=np.random.randint(4, 10))])
+            w2 = ''.join([str(x) for x in np.random.choice([0, 1], size=np.random.randint(4, 10))])
+            self.assertEqual(longest_common_subsequence(w1, w2), pylcs.lcs_sequence_length(w1, w2))
+
+    def test_concrete(self):
+        self.assertTrue(longest_common_subsequence('', '') == 0)
+        self.assertTrue(longest_common_subsequence('', '1') == 0)
+        self.assertTrue(longest_common_subsequence('1', '') == 0)
+        self.assertTrue(longest_common_subsequence('1', '0') == 0)
+        self.assertTrue(longest_common_subsequence('0', '111') == 0)
+        self.assertTrue(longest_common_subsequence('1111', '1') == 1)
+        self.assertTrue(longest_common_subsequence('110001', '1000101') == 5)
+        self.assertTrue(longest_common_subsequence('1111', '101010') == 3)
+        self.assertTrue(longest_common_subsequence('11001', '100010') == 4)
+        self.assertTrue(longest_common_subsequence('1010', '11010') == 4)
+        self.assertTrue(longest_common_subsequence('110001', '0000010') == 4)
+        self.assertTrue(longest_common_subsequence('110000', '0100010') == 5)
+        self.assertTrue(longest_common_subsequence('100010', '110001') == 5)
+        self.assertTrue(longest_common_subsequence('101010', '111111') == 3)
+        self.assertTrue(longest_common_subsequence('11010', '11001') == 4)
+        self.assertTrue(longest_common_subsequence('0010010', '110001') == 4)
 
