1b2e5a1b696b24a68cdbf226146a7296eb243507
Orel Adivi <orel.adivi@cs.technion.ac.il>
2024-04-13 20:24:20 +0300

    Run unittests

--- experiments/Unittests.py
+++ experiments/Unittests.py
@@ -1,16 +1,21 @@
 #
 #   @file : Unittests.py
-#   @date : 05 March 2024
+#   @date : 13 April 2024
 #   @authors : Orel Adivi and Daniel Noor
 #
 import unittest
 import pylcs
-from utils.LevenshteinDistance import *
-from utils.LongestCommonSubsequence import *
+import numpy as np
+
+from utils.LevenshteinDistance import levenshtein_deletion_distance
+from utils.LongestCommonSubsequence import longest_common_subsequence
 
 
-class TestLevenshteinDistance(unittest.TestCase):
-    def test_equal_lengths(self):
+class TestImplementationCorrectness(unittest.TestCase):
+    """The implementation correctness of levenshtein_deletion_distance and
+    longest_common_subsequence is checked in this unittest class"""
+
+    def test_levenshtein_distance_equal_lengths(self):
         self.assertTrue(levenshtein_deletion_distance('', '') == 0)
         self.assertTrue(levenshtein_deletion_distance('1', '0') == 2)
         self.assertTrue(levenshtein_deletion_distance('1111', '0000') == 8)
@@ -21,7 +26,7 @@ class TestLevenshteinDistance(unittest.TestCase):
         self.assertTrue(levenshtein_deletion_distance('0000010', '1100001') == 4)
         self.assertTrue(levenshtein_deletion_distance('0100010', '1100100') == 4)
 
-    def test_longer_expected(self):
+    def test_levenshtein_distance_longer_expected(self):
         self.assertTrue(levenshtein_deletion_distance('1', '') == 1)
         self.assertTrue(levenshtein_deletion_distance('111', '0') == 4)
         self.assertTrue(levenshtein_deletion_distance('1111', '1') == 3)
@@ -32,7 +37,7 @@ class TestLevenshteinDistance(unittest.TestCase):
         self.assertTrue(levenshtein_deletion_distance('0000010', '110001') == 5)
         self.assertTrue(levenshtein_deletion_distance('0100010', '110000') == 3)
 
-    def test_longer_measured(self):
+    def test_levenshtein_distance_longer_measured(self):
         self.assertTrue(levenshtein_deletion_distance('', '1') == 1)
         self.assertTrue(levenshtein_deletion_distance('0', '111') == 4)
         self.assertTrue(levenshtein_deletion_distance('1', '1111') == 3)
@@ -43,15 +48,13 @@ class TestLevenshteinDistance(unittest.TestCase):
         self.assertTrue(levenshtein_deletion_distance('110001', '0000010') == 5)
         self.assertTrue(levenshtein_deletion_distance('110000', '0100010') == 3)
 
-
-class TestLCS(unittest.TestCase):
-    def test_random(self):
+    def test_lcs_random(self):
         for i in range(10):
             w1 = ''.join([str(x) for x in np.random.choice([0, 1], size=np.random.randint(4, 10))])
             w2 = ''.join([str(x) for x in np.random.choice([0, 1], size=np.random.randint(4, 10))])
             self.assertEqual(longest_common_subsequence(w1, w2), pylcs.lcs_sequence_length(w1, w2))
 
-    def test_concrete(self):
+    def test_lcs_concrete(self):
         self.assertTrue(longest_common_subsequence('', '') == 0)
         self.assertTrue(longest_common_subsequence('', '1') == 0)
         self.assertTrue(longest_common_subsequence('1', '') == 0)
@@ -68,4 +71,3 @@ class TestLCS(unittest.TestCase):
         self.assertTrue(longest_common_subsequence('101010', '111111') == 3)
         self.assertTrue(longest_common_subsequence('11010', '11001') == 4)
         self.assertTrue(longest_common_subsequence('0010010', '110001') == 4)
-
